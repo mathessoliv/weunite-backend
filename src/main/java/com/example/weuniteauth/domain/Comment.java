22fdb5b5298656f864bddd620f7150cfa694817e
luizgusmao07 <lgtgusmao@gmail.com>
2025-08-12 17:58:20 -0300

    fix:removendo herança do Comment com o Post

--- src/main/java/com/example/weuniteauth/domain/Comment.java
+++ src/main/java/com/example/weuniteauth/domain/Comment.java
@@ -3,6 +3,7 @@ package com.example.weuniteauth.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +11,33 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
-public class Comment extends Post {
+public class Comment {
 
     public Comment(User user, Post post, String text, String imageUrl) {
-        setUser(user);
-        setPost(post);
-        setText(text);
-        setImageUrl(imageUrl);
+        this.user = user;
+        this.post = post;
+        this.text = text;
+        this.imageUrl = imageUrl;
     }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 500)
+    private String text;
+
+    @Column(length = 500)
+    private String imageUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
@@ -30,4 +50,14 @@ public class Comment extends Post {
 
     @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 }
