6c082badf9e90fdadbe08eb3b6abe9c6a024a0d7
matheushahah <78930361+Matheus-fast@users.noreply.github.com>
2025-08-19 18:22:17 -0300

    fix(comments): organizando comentários por data de criação decrescente

--- src/main/java/com/example/weuniteauth/repository/CommentRepository.java
+++ src/main/java/com/example/weuniteauth/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.example.weuniteauth.repository;
 
 import com.example.weuniteauth.domain.Comment;
-import com.example.weuniteauth.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +10,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPostId(Long postId);
 
+    @Query("SELECT c FROM Comment c ORDER BY COALESCE(c.updatedAt, c.createdAt) DESC")
     List<Comment> findByUserId(Long userId);
+
 }
 
 
